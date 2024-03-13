1fa6592d8cf829c64b4f0f9b33950e2700aba4d3
Alla Kostomarova <alla.kostomarova@mail.ru>
2024-03-13 20:57:23 +0200

    login success

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,40 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+    @BeforeMethod
+    public void preCondition(){
+        // if SignOut ---> logout
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logOut();
+        }
+    }
+
     @Test
-    public void test(){
-        
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("aa@aa.ru", "Test123$");
+        app.getHelperUser().submitLogin();
+
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
+
+    @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("aa@aa.ru", "Test123$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
     }
 }
